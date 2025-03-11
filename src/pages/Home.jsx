08d9f66f781f1a3980b79597034cf49a001a2566
Faldiansyah <faldiansyahk@gmail.com>
2025-03-11 08:22:52 +0700

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Home = () => {
+function Home() {
   return (
-    <div>
-      
-    </div>
+    <section className="min-h-screen flex flex-col items-center justify-center text-center">
+      <h1 className="text-4xl md:text-6xl font-bold mb-4 animate-fadeIn">Kembalikan Zaman Keemasan Islam</h1>
+      <p className="max-w-2xl mb-8">Mau tahu langkah-langkah untuk mengembalikan masa keemasan dengan menggabungkan ilmu pengetahuan, teknologi, dan ajaran Islam?</p>
+      <Link to="/tech-religion" className="btn btn-primary transition-transform duration-500 hover:scale-105">Mulai Jelajahi</Link>
+    </section>
   )
 }
 
